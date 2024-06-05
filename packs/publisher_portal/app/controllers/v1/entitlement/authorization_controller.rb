c50dd885e46e1083b4e9011995f29088dddd35d5
ghun131 <hungdh131@gmail.com>
2024-06-05 16:26:52 +0700

    feat: emit consumption event when user action is authorized

--- packs/publisher_portal/app/controllers/v1/entitlement/authorization_controller.rb
+++ packs/publisher_portal/app/controllers/v1/entitlement/authorization_controller.rb
@@ -6,7 +6,6 @@ module V1
   module Entitlement
     class AuthorizationController < ApplicationController
       def index
-        puts "authorization_params: #{authorization_params}"
         request_params_valid = Authorization::AuthorizeValidator.new(authorization_params).valid?
 
         return render(json: failure_response(code: 422, message: "Request payload error")) unless request_params_valid
@@ -17,7 +16,7 @@ module V1
 
         if authorized_result[:is_authorized]
           # create GetLago event based on authorized_result[:subscription_plan]
-          # create_get_lago_event(authorized_result[:subscription_plan], request)
+          create_get_lago_event(authorized_result[:subscription_plan], request)
 
           return render(json: success_response(message: "Authorized", extra: authorized_result[:subscription_plan]))
         end
@@ -44,6 +43,10 @@ module V1
         )
       end
 
+      def create_get_lago_event(subscription_plan, request)
+        ConsumptionEvent::EmitService.call(subscription_plan: subscription_plan, request: request)
+      end
+
       def authorize(payload)
         auth_payload = EntitlementAdapter::ConverterService.call(payload: payload, policy_store_id: policy_store_id)
         Authorization::AuthorizeService.call(payload: auth_payload, client: AwsAvp.init)
@@ -52,46 +55,6 @@ module V1
       def policy_store_id
         ENV['AUTHENTICATION_POLICY_STORE_ID']
       end
-
-      # def create_get_lago_event(subscription_plan, request)
-      #   puts "subscription_plan: #{subscription_plan}"
-      #   result = ::Events::CreateService.call(
-      #     organization: current_organization(subscription_plan[:organization_id]),
-      #     params: create_params(subscription_plan[:id], JSON.parse(request.body.read)),
-      #     timestamp: Time.current.to_f,
-      #     metadata: event_metadata(request),
-      #   )
-
-      #   if result.success?
-      #     render(
-      #       json: ::V1::EventSerializer.new(
-      #         result.event,
-      #         root_name: 'event',
-      #       ),
-      #     )
-      #   else
-      #     render_error_response(result)
-      #   end
-      # end
-
-      # def current_organization(organization_id)
-      #   puts "organization_id: #{organization_id}"
-      #   Organization.find_by(id: organization_id)
-      # end
-
-      # def event_metadata(request)
-      #   {
-      #     user_agent: request.user_agent,
-      #     ip_address: request.remote_ip,
-      #   }
-      # end
-
-      # def create_params(plan_id, req_payload)
-      #   puts "plan_id: #{plan_id}"
-      #   puts "req_payload: #{req_payload}"
-      #   subscription = Subscription.find_by(plan_id: plan_id, customer_id: req_payload["userId"])
-      #   puts "subscription: #{subscription.to_json}"
-      # end
 
       def failure_response(code: 401, message: "Unauthorized", extra: {})
         {
---
+++ packs/publisher_portal/app/services/consumption_event/emit_service.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+module ConsumptionEvent
+  class EmitService < BaseService
+    def initialize(subscription_plan:, request:)
+      @subscription_plan = subscription_plan
+      @request = request
+
+      super
+    end
+
+    def call
+      validate_subscription_plan(subscription_plan)
+
+      event_params = create_params(subscription_plan["id"], JSON.parse(request.body.read))
+
+      result = ::Events::CreateService.call(
+        organization: current_organization(subscription_plan["organization_id"]),
+        params: event_params,
+        timestamp: Time.current.to_f,
+        metadata: event_metadata(request),
+      )
+
+      if result.success?
+        ::V1::EventSerializer.new(
+          result.event,
+          root_name: 'event',
+        )
+      else
+        render_error_response(result)
+      end
+    end
+
+    attr_reader :subscription_plan, :request
+
+    private
+
+    def validate_subscription_plan(subscription_plan)
+      ["id", "organization_id"].each do |key|
+        unless subscription_plan.has_key?(key) && subscription_plan[key].is_a?(String)
+          raise ArgumentError, "'#{key}' must be present and must be a string"
+        end
+      end
+    end
+
+    def current_organization(organization_id)
+      Organization.find_by(id: organization_id)
+    end
+
+    def event_metadata(request)
+      {
+        user_agent: request.user_agent,
+        ip_address: request.remote_ip,
+      }
+    end
+
+    def create_params(plan_id, req_payload)
+      customer = Customer.find_by(id: req_payload["userId"])
+      subscription = Subscription.find_by(plan_id: plan_id, customer_id: customer.id)
+
+      # TODO: a plan can have multiple billable metrics. Do we emit one consumption event per billable metric?
+      billable_metric = subscription.plan.billable_metrics.first
+      external_subscription_id = subscription.external_id
+      external_customer_id = customer.external_id
+
+      {
+        transaction_id: SecureRandom.uuid,
+        external_customer_id: external_customer_id,
+        code: billable_metric.code,
+        external_subscription_id: external_subscription_id,
+        timestamp: Time.current.to_f
+      }
+    end
+  end
+end
