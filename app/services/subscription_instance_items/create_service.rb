681949b6ee615019dbe284a3d5962e752fa20ce1
TronTram <txtron2012@gmail.com>
2024-06-08 23:41:18 +0700

    add create_service to create subscription_instance_item

---
+++ app/services/subscription_instance_items/create_service.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module SubscriptionInstanceItems
+  class CreateService < BaseService
+    def initialize(subscription_instance:, fee_amount_cents:, charge_type:, code: nil)
+      @subscription_instance = subscription_instance
+      @fee_amount_cents = fee_amount_cents
+      @charge_type = charge_type
+      @code = code
+
+      super
+    end
+
+    def call
+      result.subscription_instance_item = subscription_instance.subscription_instance_items.create!(
+        fee_amount_cents:,
+        charge_type:,
+        code:
+      )
+
+      result
+    rescue ActiveRecord::RecordInvalid => e
+      result.record_validation_failure!(record: e.record)
+    rescue ArgumentError
+      result.validation_failure!(errors: { charge_type: ['invalid_charge_type'] })
+    end
+
+    private
+
+    attr_reader :subscription_instance, :fee_amount_cents, :charge_type, :code
+  end
+end
