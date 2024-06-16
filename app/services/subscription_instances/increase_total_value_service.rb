540942e78da690f5c696b38a67bbf2f397cf4515
TronTram <txtron2012@gmail.com>
2024-06-16 21:43:13 +0700

    add IncreaseTotalValueService for subscription instance

---
+++ app/services/subscription_instances/increase_total_value_service.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module SubscriptionInstances
+  class IncreaseTotalValueService < BaseService
+    def initialize(subscription_instance:, fee_amount:)
+      @subscription_instance = subscription_instance
+      @fee_amount = fee_amount
+
+      super
+    end
+
+    def call
+      return unless fee_amount.positive?
+
+      ActiveRecord::Base.transaction do
+        subscription_instance.lock!
+        subscription_instance.total_amount += fee_amount
+        subscription_instance.version_number += 1
+        subscription_instance.save!
+
+        result.subscription_instance = subscription_instance.reload
+      end
+
+      result
+    rescue ActiveRecord::RecordInvalid => e
+      result.record_validation_failure!(record: e.record)
+    end
+
+    private
+
+    attr_reader :subscription_instance, :fee_amount
+  end
+end
