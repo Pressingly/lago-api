5d3cfc89deeb6f30e062a7736345e7089370e5cb
ghun131 <hungdh131@gmail.com>
2024-06-17 12:25:32 +0700

    fix: rb generated files by grpc to packs/publisher_portal/lib

--- packs/publisher_portal/app/services/subscription_charges/create_service.rb
+++ packs/publisher_portal/app/services/subscription_charges/create_service.rb
@@ -4,31 +4,13 @@ require_relative '../../../../../grpc/publisher_revenue/client/lib/subscription_
 
 module SubscriptionCharges
   class CreateService < BaseService
-    def initialize(authorized_result:, request_payload:)
-      @authorized_result = authorized_result
-      @request_payload = request_payload
+    def initialize(subscription:)
+      @subscription = subscription
 
       super
     end
 
     def call
-      plan_id = authorized_result[:subscription_plan]["id"]
-
-      user_id = request_payload["userId"]
-
-      subscription = Subscription
-        .joins(:plan, :customer)
-        .where(
-          customer_id: user_id,
-          plan_id: plan_id,
-          terminated_at: nil,
-          canceled_at: nil
-        ).first
-      puts "subscription: #{subscription.inspect}"
-      sub_instance = SubscriptionInstances::CreateService.new(subscription: subscription).call
-
-      puts "subscription_instance:  #{sub_instance}"
-
       hostname = 'http://localhost:5002'
       stub = SubscriptionCharge::SubscriptionChargeGrpcService::Stub.new(hostname)
 
@@ -40,7 +22,7 @@ module SubscriptionCharges
       ))
     end
 
-    attr_reader :authorized_result, :request_payload
+    attr_reader :subscription
 
     private
   end
