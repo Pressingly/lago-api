949d071f4292b4d2c08aa7b21e2e7bd9eb1fc9f4
ghun131 <hungdh131@gmail.com>
2024-06-17 18:08:13 +0700

    feat: integrate with publisher revenue using grpc

--- packs/publisher_portal/app/services/subscription_charges/create_service.rb
+++ packs/publisher_portal/app/services/subscription_charges/create_service.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative '../../../../../grpc/publisher_revenue/client/lib/subscription_charge.service_services_pb'
+require 'revenue.service_services_pb'
 
 module SubscriptionCharges
   class CreateService < BaseService
@@ -11,19 +11,31 @@ module SubscriptionCharges
     end
 
     def call
-      hostname = 'http://localhost:5002'
-      stub = SubscriptionCharge::SubscriptionChargeGrpcService::Stub.new(hostname)
+      sub_instance = SubscriptionInstance.find_by(subscription_id: subscription.id)
+      customer = Customer.find_by(id: subscription.customer_id)
+      charge = Charge.where(plan_id: subscription.plan_id).first
 
-      stub.create_subscription_charge(SubscriptionCharge::CreateSubscriptionChargeReq.new(
-        pinetIdToken: 'pinetIdToken',
-        pinetUserId: 'pinetUserId',
-        membershipOrgId: 'membershipOrgId',
-        subscriptionInstanceId: sub_instance.id
+      # TODO: dynamically set description and fraction
+      stub.create_subscription_charge(Revenue::CreateSubscriptionChargeReq.new(
+        {
+          amount: sub_instance.total_amount,
+          currencyCode: charge.amount_currency,
+          description: "eu reprehenderit aliquip",
+          fraction: 2,
+          pinetIdToken: customer.pinet_id_token,
+          subscriptionInstanceId: sub_instance.id,
+        }
       ))
     end
 
     attr_reader :subscription
 
     private
+
+    def stub
+      Revenue::RevenueGrpcService::Stub.new(ENV['PUBLISHER_REVENUE_GRPC_URL'], :this_channel_is_insecure)
+    rescue GRPC::BadStatus => e
+      abort "ERROR: #{e.message}"
+    end
   end
 end
