796ce2436d209dada3fc97f8f501d39ca9ef3f28
ghun131 <hungdh131@gmail.com>
2024-06-18 17:57:59 +0700

    fix: update proto file

--- packs/publisher_portal/app/services/subscription_charges/create_service.rb
+++ packs/publisher_portal/app/services/subscription_charges/create_service.rb
@@ -13,15 +13,13 @@ module SubscriptionCharges
     def call
       sub_instance = SubscriptionInstance.find_by(subscription_id: subscription.id)
       customer = Customer.find_by(id: subscription.customer_id)
-      charge = Charge.where(plan_id: subscription.plan_id).first
 
       # TODO: dynamically set description and fraction
       stub.create_subscription_charge(Revenue::CreateSubscriptionChargeReq.new(
         {
-          amount: sub_instance.total_amount,
-          currencyCode: charge.amount_currency,
+          amount: sub_instance.total_amount.to_i,
+          currencyCode: customer.currency,
           description: "eu reprehenderit aliquip",
-          fraction: 2,
           pinetIdToken: customer.pinet_id_token,
           subscriptionInstanceId: sub_instance.id,
         }
