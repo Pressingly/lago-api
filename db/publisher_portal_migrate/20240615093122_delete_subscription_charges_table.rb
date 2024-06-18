908d80a0d2bddc83b58e011c0cfe7fabe5daad64
ghun131 <hungdh131@gmail.com>
2024-06-18 17:18:38 +0700

    chore: fix lint

--- db/publisher_portal_migrate/20240615093122_delete_subscription_charges_table.rb
+++ db/publisher_portal_migrate/20240615093122_delete_subscription_charges_table.rb
@@ -1,5 +1,5 @@
 class DeleteSubscriptionChargesTable < ActiveRecord::Migration[7.0]
   def change
-    drop_table :subscription_charges
+    drop_table :subscription_charges # rubocop:disable Rails/ReversibleMigration
   end
 end
