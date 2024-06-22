889deeb38e992f1b66962f03347afa665c471262
TronTram <txtron2012@gmail.com>
2024-06-22 20:09:46 +0700

    add test case for race condition

--- spec/services/subscription_instances/increase_total_value_service_spec.rb
+++ spec/services/subscription_instances/increase_total_value_service_spec.rb
@@ -3,30 +3,35 @@
 require 'rails_helper'
 
 RSpec.describe SubscriptionInstances::IncreaseTotalValueService, type: :service do
-  let(:subscription_instance) { create(:subscription_instance, total_amount: 1000, version_number: 1) }
-  let(:service) { described_class.new(subscription_instance:, fee_amount: 500) }
+  let(:initial_total_amount) { 100 }
+  let(:fee_amount) { 50 }
+  let(:subscription_instance) { create(:subscription_instance, total_amount: initial_total_amount, version_number: 1) }
+  let(:service) { described_class.new(subscription_instance:, fee_amount:) }
 
   describe '#call' do
     context 'when the service is called successfully' do
+      let(:thread_count) { 3 }
+
       it 'increases the total subscription value' do
         result = service.call
 
-        expect(result.subscription_instance.total_amount).to eq(1500)
+        expect(result.subscription_instance.total_amount).to eq(initial_total_amount + fee_amount)
         expect(result.subscription_instance.version_number).to eq(2)
       end
-    end
 
-    context 'when the update fails' do
-      before do
-        allow(subscription_instance).to receive(:save!).and_raise(ActiveRecord::RecordInvalid.new(subscription_instance))
-      end
+      it 'updates total_amount correctly with concurrent threads' do
+        threads = Array.new(thread_count) do
+          Thread.new do
+            sleep(rand(0.1..0.5)) # Simulate some processing time
+            described_class.new(subscription_instance: subscription_instance, fee_amount: fee_amount).call
+          end
+        end
 
-      it 'does not increase the total subscription value' do
-        result = service.call
+        threads.each(&:join) # Wait for all threads to complete
+
         subscription_instance.reload
 
-        expect(result.success?).to be(false)
-        expect(subscription_instance.total_amount).to eq(1000)
+        expect(subscription_instance.total_amount).to eq(initial_total_amount + fee_amount * thread_count)
       end
     end
   end
