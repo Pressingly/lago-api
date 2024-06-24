afe72a3384586494557e9832bc08f0081ef96fa2
ghun131 <hungdh131@gmail.com>
2024-06-24 20:23:13 +0700

    fix: log payload subscription_charge creation

--- packs/publisher_portal/app/services/subscription_charges/create_service.rb
+++ packs/publisher_portal/app/services/subscription_charges/create_service.rb
@@ -13,17 +13,19 @@ module SubscriptionCharges
     def call
       customer = Customer.joins(:subscriptions)
         .find_by(subscriptions: { id: subscription_instance.subscription_id})
+      plan = Plan.joins(:subscriptions)
+        .find_by(subscriptions: {id: subscription_instance.subscription_id})
+      payload = {
+        amount: subscription_instance.total_amount.to_f,
+        currencyCode: customer.currency,
+        versionNumber: subscription_instance.version_number,
+        description: plan.description,
+        pinetIdToken: customer.pinet_id_token,
+        subscriptionInstanceId: subscription_instance.id,
+      }
 
-      stub.create_subscription_charge(Revenue::CreateSubscriptionChargeReq.new(
-        {
-          amount: subscription_instance.total_amount.to_f,
-          currencyCode: customer.currency,
-          versionNumber: subscription_instance.version_number,
-          description: plan.description,
-          pinetIdToken: customer.pinet_id_token,
-          subscriptionInstanceId: subscription_instance.id,
-        }
-      ))
+      stub.create_subscription_charge(Revenue::CreateSubscriptionChargeReq.new(payload))
+      Rails.logger.info("Subcription charge creation payload: #{payload}")
     rescue GRPC::BadStatus => e
       raise StandardError, "Error creating subscription charge: #{e.message}"
     end
