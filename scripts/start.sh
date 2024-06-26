dbfbc459d802f46b98515bd21391b0433761fd8c
wormym1011 <80243096+wormym1011@users.noreply.github.com>
2024-06-26 22:37:42 +0700

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-
-if [ "$RAILS_ENV" == "staging" ]
-then
-  bundle exec rake db:prepare
-fi
-
-rm -f ./tmp/pids/server.pid
-bundle exec rails db:migrate:primary
-
-if [ -v LAGO_CLICKHOUSE_ENABLED ] && [ "$LAGO_CLICKHOUSE_ENABLED" == "true" ]
-then
-  bundle exec rake db:migrate:clickhouse
-fi
-
+bundle exec rake db:create
+bundle exec rake db:prepare
+bundle exec rails db:migrate
 bundle exec rails signup:seed_organization
 bundle exec rails s -b ::
