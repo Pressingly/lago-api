a6d56ee3a12ef9986f7edcaf99a594c7b5e16df8
ghun131 <hungdh131@gmail.com>
2024-06-26 17:26:10 +0700

    feat: grpc auth server for Publisher Revenue

--- config.ru
+++ config.ru
@@ -2,7 +2,17 @@
 
 # This file is used by Rack-based servers to start the application.
 
+require 'grpc'
 require_relative 'config/environment'
+require_relative 'packs/publisher_portal/lib/auth_token_services_pb'
+require_relative 'grpc/publisher_revenue/server/auth_token'
+
+Thread.new do
+  s = GRPC::RpcServer.new
+  s.add_http2_port('0.0.0.0:50051', :this_port_is_insecure)
+  s.handle(AuthService)
+  s.run_till_terminated
+end
 
 run Rails.application
 Rails.application.load_server
---
+++ grpc/publisher_revenue/server/auth_token.rb
@@ -0,0 +1,34 @@
+# services/auth_service.rb
+
+require 'grpc'
+require 'auth_token_services_pb'
+
+class AuthService < Auth::Token::Service
+  def verify(token_req, _unused_call)
+    if token_req.token && decoded_token(token_req.token) && valid_token?
+      Auth::AuthResponse.new(status: "succeed")
+    else
+      Auth::AuthResponse.new(status: "failed")
+    end
+  end
+
+  private
+
+  def decoded_token(token)
+    decoded_token ||= JWT.decode(token, ENV['SECRET_KEY_BASE'], true, decode_options)
+    @payload = decoded_token.reduce({}, :merge)
+  rescue JWT::DecodeError => e
+    Rails.logger.error("Error decoding token: #{e}")
+    raise e if e.is_a?(JWT::ExpiredSignature) || Rails.env.development?
+  end
+
+  def valid_token?
+    Time.now.to_i <= @payload['exp']
+  end
+
+  def decode_options
+    {
+      algorithm: 'HS256',
+    }
+  end
+end
