27193246233e14c7a1493d9573d381420e4a13f5
TronTram <txtron2012@gmail.com>
2024-06-28 08:31:16 +0700

    add two synchronous service for event

---
+++ app/services/events/sync/create_sync_service.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Events
+  module Sync
+    class CreateSyncService < Events::CreateService
+      def call
+        event = Event.new
+        event.organization_id = organization.id
+        event.code = params[:code]
+        event.transaction_id = params[:transaction_id]
+        event.external_customer_id = params[:external_customer_id]
+        event.external_subscription_id = params[:external_subscription_id]
+        event.properties = params[:properties] || {}
+        event.metadata = metadata || {}
+        event.timestamp = Time.zone.at(params[:timestamp] ? params[:timestamp].to_f : timestamp)
+        event.save!
+
+        result.event = event
+
+        produce_kafka_event(event)
+        Events::PostProcessJob.perform_now(event:)
+
+        result
+      rescue ActiveRecord::RecordInvalid => e
+        result.record_validation_failure!(record: e.record)
+      rescue ActiveRecord::RecordNotUnique
+        result.single_validation_failure!(field: :transaction_id, error_code: 'value_already_exist')
+      end
+    end
+  end
+end
