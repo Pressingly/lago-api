feea9cbfc014f4bdc6ebe5459823dd1f7dd153f9
ghun131 <hungdh131@gmail.com>
2024-06-28 11:51:28 +0700

    feat: integrate finalize subscription_charge with subscription_instance

--- app/jobs/subscription_instances/finalize_job.rb
+++ app/jobs/subscription_instances/finalize_job.rb
@@ -5,14 +5,16 @@ module SubscriptionInstances
     queue_as 'billing'
 
     def perform(subscription_instance:, subscription_fee:, charges_fees:)
-      result = SubscriptionInstances::FinalizeService.new(
-        subscription_instance: subscription_instance,
-        subscription_fee: subscription_fee,
-        charges_fees: charges_fees
-      ).call
+      ActiveRecord::Base.transaction do
+        result = SubscriptionInstances::FinalizeService.new(
+          subscription_instance: subscription_instance,
+          subscription_fee: subscription_fee,
+          charges_fees: charges_fees
+        ).call
 
-      result.raise_if_error!
-      # TODO: Call service to finalize subscription charge
+        result.raise_if_error!
+        SubscriptionCharges::FinalizeService.call(subscription_instance: subscription_instance)
+      end
     end
   end
 end
