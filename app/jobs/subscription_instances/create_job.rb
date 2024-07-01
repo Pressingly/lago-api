1413f7f740ad01748172516491c59780b71ab85d
TronTram <txtron2012@gmail.com>
2024-07-01 18:56:59 +0700

    refactor SubscriptionInstances::CreateJob and SubscriptionInstances::Helper

--- app/jobs/subscription_instances/create_job.rb
+++ app/jobs/subscription_instances/create_job.rb
@@ -1,36 +1,38 @@
 # frozen_string_literal: true
 
-class SubscriptionInstances::CreateJob < ApplicationJob
-  include SubscriptionInstances::Helper
-  queue_as 'billing'
+module SubscriptionInstances
+  class CreateJob < ApplicationJob
+    include SubscriptionInstances::Helper
+    queue_as 'billing'
 
-  def perform(subscription)
-    boundaries = date_service(subscription)
-    ActiveRecord::Base.transaction do
-      result = SubscriptionInstances::CreateService.new(
-        subscription:,
-        started_at: boundaries.from_datetime,
-        ended_at: boundaries.to_datetime
-      ).call
+    def perform(subscription)
+      boundaries = date_service(subscription)
+      ActiveRecord::Base.transaction do
+        result = SubscriptionInstances::CreateService.new(
+          subscription:,
+          started_at: boundaries.from_datetime,
+          ended_at: boundaries.to_datetime
+        ).call
 
-      result.raise_if_error!
+        result.raise_if_error!
 
-      if should_create_subscription_charge?(result)
-        SubscriptionCharges::CreateService.call(
-          subscripton_instance: result.subscription_instance,
-          subscription_instance_item: result.subscription_instance_item
-        )
+        if should_create_subscription_charge?(result)
+          SubscriptionCharges::CreateService.call(
+            subscripton_instance: result.subscription_instance,
+            subscription_instance_item: result.subscription_instance_item
+          )
+        end
       end
     end
-  end
 
-  private
+    private
 
-  def date_service(subscription)
-    Subscriptions::DatesService.new_instance(
-      subscription,
-      subscription.started_at,
-      current_usage: true
-    )
+    def date_service(subscription)
+      Subscriptions::DatesService.new_instance(
+        subscription,
+        subscription.started_at,
+        current_usage: true
+      )
+    end
   end
 end
--- app/jobs/subscription_instances/helper.rb
+++ app/jobs/subscription_instances/helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module SubscriptionInstances
-  class Helper
+  module Helper
     private
 
     def should_create_subscription_charge?(result)
