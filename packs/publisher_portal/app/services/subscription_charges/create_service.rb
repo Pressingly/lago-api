db48939b26e71faff7bce4902a53404b288afed0
TronTram <txtron2012@gmail.com>
2024-07-01 14:46:05 +0700

    call service to update contract status of s-instance-item and total_amount of s-instance when creating or updating subscription charge successfully

--- packs/publisher_portal/app/services/subscription_charges/create_service.rb
+++ packs/publisher_portal/app/services/subscription_charges/create_service.rb
@@ -4,19 +4,20 @@ module SubscriptionCharges
   class CreateService < BaseService
     include ServiceHelper
 
-    def initialize(subscription_instance:)
+    def initialize(subscription_instance:, subscription_instance_item:)
       @subscription_instance = subscription_instance
+      @subscription_instance_item = subscription_instance_item
 
       super
     end
 
-    attr_reader :subscription_instance
+    attr_reader :subscription_instance, :subscription_instance_item
 
     def call
       customer = subscription_instance.subscription.customer
       plan = subscription_instance.subscription.plan
       payload = {
-        amount: subscription_instance.total_amount.to_f,
+        amount: subscription_instance_item.fee_amount.to_f,
         currencyCode: customer.currency,
         versionNumber: subscription_instance.version_number,
         description: plan.description,
@@ -24,10 +25,30 @@ module SubscriptionCharges
         subscriptionInstanceId: subscription_instance.id,
       }
 
-      stub.create_subscription_charge(Revenue::CreateSubscriptionChargeReq.new(payload))
+      subscription_charge_result = stub.create_subscription_charge(Revenue::CreateSubscriptionChargeReq.new(payload))
+
+      # TODO: check response status
+      if subscription_charge_result["status"] == "approved"
+        subscription_instance_item.approve!
+        update_subscription_instance_total_amount(subscription_instance_item.fee_amount)
+      elsif subscription_charge_result["status"] == "rejected"
+        subscription_instance_item.reject!
+      end
+
       Rails.logger.info("Subcription charge creation payload: #{payload}")
     rescue GRPC::BadStatus => e
       result.service_failure!(code: 'grpc_failed', error_message: "updating subscription charge: #{e.message}")
+    end
+
+    private
+
+    def update_subscription_instance_total_amount(fee_amount)
+      return unless fee_amount.positive?
+
+      SubscriptionInstances::IncreaseTotalValueService.call(
+        subscription_instance:,
+        fee_amount:
+      ).raise_if_error!
     end
   end
 end
