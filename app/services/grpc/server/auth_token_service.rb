d4a5b2f94b5a3e6bb26fa5d53338ef9b7f1a2284
ghun131 <hungdh131@gmail.com>
2024-07-02 14:31:10 +0700

    refactor: only use valid_token?

--- app/services/grpc/server/auth_token_service.rb
+++ app/services/grpc/server/auth_token_service.rb
@@ -7,7 +7,7 @@ module Grpc
   module Server
     class AuthTokenService < Auth::Token::Service
       def verify(token_req, _unused_call)
-        if token_req.token && decoded_token(token_req.token) && valid_token?
+        if token_req.token && valid_token?(token_req.token)
           Auth::AuthResponse.new(status: "succeed")
         else
           Auth::AuthResponse.new(status: "failed")
@@ -16,16 +16,13 @@ module Grpc
 
       private
 
-      def decoded_token(token)
+      def valid_token?(token)
         decoded_token ||= JWT.decode(token, ENV['SECRET_KEY_BASE'], true, decode_options)
         @payload = decoded_token.reduce({}, :merge)
+        Time.now.to_i <= @payload['exp']
       rescue JWT::DecodeError => e
         Rails.logger.error("Error decoding token: #{e}")
         raise e if e.is_a?(JWT::ExpiredSignature) || Rails.env.development?
-      end
-
-      def valid_token?
-        Time.now.to_i <= @payload['exp']
       end
 
       def decode_options
