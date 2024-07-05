0d067bbe1bcc894ab0bbe021bab58cfb18bae9e6
TronTram <txtron2012@gmail.com>
2024-07-05 08:47:51 +0700

    chore: Add LAGO_API_HOST to staging environment configuration

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -44,6 +44,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.hosts << /[a-z0-9-]+\.staging\.getlago\.com/
+  config.hosts << ENV['LAGO_API_HOST'] if ENV['LAGO_API_HOST'].present?
 
   config.license_url = 'http://license-staging-web.default.svc.cluster.local'
 
