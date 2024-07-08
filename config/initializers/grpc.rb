601e0f4c94cf39b6988e27afe775a6a2efea62ad
TronTram <txtron2012@gmail.com>
2024-07-08 12:04:07 +0700

    Call GRPC.prefork before initializing the GRPC server in a new thread

--- config/initializers/grpc.rb
+++ config/initializers/grpc.rb
@@ -1,6 +1,8 @@
 require_relative '../../packs/publisher_portal/lib/auth_token_services_pb'
 require_relative '../../app/services/grpc/server/auth_token_service'
 
+GRPC.prefork
+
 Thread.new do # rubocop:disable ThreadSafety/NewThread
   s = GRPC::RpcServer.new
   s.add_http2_port(ENV['GRPC_HOST'] + ":" + ENV['GRPC_PORT'], :this_port_is_insecure)
