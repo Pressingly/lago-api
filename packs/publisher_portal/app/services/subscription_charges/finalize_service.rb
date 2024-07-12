c34bccc9acbfe1819e168891674bd16808a43c58
TronTram <txtron2012@gmail.com>
2024-07-12 21:54:03 +0700

    refactor subscription charge finalize service

--- packs/publisher_portal/app/services/subscription_charges/finalize_service.rb
+++ packs/publisher_portal/app/services/subscription_charges/finalize_service.rb
@@ -4,9 +4,8 @@ module SubscriptionCharges
   class FinalizeService < BaseService
     include ServiceHelper
 
-    def initialize(subscription_instance:, subscription_instance_items: [])
+    def initialize(subscription_instance:)
       @subscription_instance = subscription_instance
-      @subscription_instance_items = subscription_instance_items
 
       super
     end
@@ -17,27 +16,18 @@ module SubscriptionCharges
       payload = {
         subscriptionChargeId: subscription_instance.pinet_subscription_charge_id,
         versionNumber: subscription_instance.version_number,
-        amount: total_amount.to_f,
+        amount: subscription_instance.total_amount.to_f,
         currencyCode: customer.currency,
         description: plan.description,
         pinetIdToken: customer.pinet_id_token,
       }
 
+      Rails.logger.info("Subcription charge finalization payload: #{payload}")
       finalize_result = stub.finalize_subscription_charge(Revenue::FinalizeSubscriptionChargeReq.new(payload))
 
-      # TODO: depending on finalize api, we may need to handle different ways
-      # for now, i assume that the finalize api allow updating total mount of subscription charge
-      if finalize_result.success
-        ActiveRecord::Base.transaction do
-          subscription_instance_items.each do |item|
-            item.approve!
-          end
-        end
-
-        finalize_subscription_instance
+      if finalize_result&.status == `SUBSCRIPTION_CHARGE_CONTRACT_STATUS_REJECTED`
+        subscription_instance.finalize!
       end
-      Rails.logger.info("Subcription charge finalization payload: #{payload}")
-      stub.finalize_subscription_charge(Revenue::FinalizeSubscriptionChargeReq.new(payload))
 
       result
     rescue GRPC::BadStatus => e
@@ -46,18 +36,6 @@ module SubscriptionCharges
 
     private
 
-    attr_reader :subscription_instance, :subscription_instance_items
-
-    def total_amount
-      subscription_instance_items.sum(&:fee_amount)
-    end
-
-    def finalize_subscription_instance
-      SubscriptionInstances::IncreaseTotalValueService.call(
-        subscription_instance: subscription_instance,
-        fee_amount: total_amount
-      )
-      subscription_instance.finalize!
-    end
+    attr_reader :subscription_instance
   end
 end
