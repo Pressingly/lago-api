384e47d11343b63b7fc53b4166df56f93f839b13
TronTram <txtron2012@gmail.com>
2024-07-12 22:09:06 +0700

    remove unused code

--- spec/jobs/subscription_instances/finalize_job_spec.rb
+++ spec/jobs/subscription_instances/finalize_job_spec.rb
@@ -6,12 +6,9 @@ RSpec.describe SubscriptionInstances::FinalizeJob, type: :job do
   describe '#perform' do
     let(:subscription_instance) { create(:subscription_instance) }
     let(:result) { BaseService::Result.new }
-    let(:subscription_fee) { create(:fee, fee_type: 'subscription') }
-    let(:charge_fee) { create(:charge_fee) }
-    let(:charges_fees) { [charge_fee] }
 
     it 'calls the finalize service with correct parameters' do
-      described_class.perform_now(subscription_instance:, subscription_fee:, charges_fees:)
+      described_class.perform_now(subscription_instance:)
       expect(finalize_service).to have_received(:call)
     end
   end
