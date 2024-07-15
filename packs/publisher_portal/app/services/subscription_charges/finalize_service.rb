b3acda6b720cb7636e1dff8c15fceb2dd7b381b5
TronTram <txtron2012@gmail.com>
2024-07-15 18:36:30 +0700

    fix: undefined total_amount

--- packs/publisher_portal/app/services/subscription_charges/finalize_service.rb
+++ packs/publisher_portal/app/services/subscription_charges/finalize_service.rb
@@ -15,7 +15,7 @@ module SubscriptionCharges
       plan = subscription_instance.subscription.plan
       payload = {
         subscriptionChargeId: subscription_instance.pinet_subscription_charge_id,
-        amount: total_amount.to_f,
+        amount: subscription_instance.total_amount.to_f,
         currencyCode: customer.currency,
         description: plan.description,
         pinetIdToken: customer.pinet_id_token,
