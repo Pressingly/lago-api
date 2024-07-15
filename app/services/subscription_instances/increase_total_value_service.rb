c63c11f5e0443c070f39cce874fdd85364b5f588
TronTram <txtron2012@gmail.com>
2024-07-15 18:02:43 +0700

    remove version_number column

--- app/services/subscription_instances/increase_total_value_service.rb
+++ app/services/subscription_instances/increase_total_value_service.rb
@@ -15,7 +15,6 @@ module SubscriptionInstances
       ActiveRecord::Base.transaction do
         subscription_instance.lock!
         subscription_instance.total_amount += fee_amount
-        subscription_instance.version_number += 1
         subscription_instance.save!
 
         result.subscription_instance = subscription_instance.reload
