9ec0e91dcabc6ed1dde6b2b06a7a7c34c5289318
ghun131 <hungdh131@gmail.com>
2024-07-15 14:30:28 +0700

    test: run migration for test db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_01_034635) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_12_123121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
