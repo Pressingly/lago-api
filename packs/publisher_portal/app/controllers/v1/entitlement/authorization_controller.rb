2ec7c7fd08a921d946d0d5177db1fe6a34f678a6
ghun131 <hungdh131@gmail.com>
2024-07-15 14:12:52 +0700

    fix: authorization controller extends api base controller

--- packs/publisher_portal/app/controllers/v1/entitlement/authorization_controller.rb
+++ packs/publisher_portal/app/controllers/v1/entitlement/authorization_controller.rb
@@ -4,7 +4,7 @@ require 'aws-sdk-verifiedpermissions'
 
 module V1
   module Entitlement
-    class AuthorizationController < ApplicationController
+    class AuthorizationController < Api::BaseController
       before_action :set_customer
       def index
         # TODO: refactor AuthorizeValidator to include customer validation
